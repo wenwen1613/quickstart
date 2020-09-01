6edab67b8dc9dc48779c3c6f842dab9c24624fec
Oops <lijingwen01@koolearn.com>
2020-09-01 18:48:25 +0800

    commit

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"github.com/astaxie/beego/context"
 	"quickstart/controllers"
 
 	"github.com/astaxie/beego"
@@ -8,9 +9,13 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
+	beego.Router("/get/example", &controllers.MainController{}, "GET:GetTest")
 	beego.Router("/user", &controllers.UserController{})
 	beego.Router("/user/info", &controllers.UserController{}, "GET:Info")
 	beego.Router("/user/list", &controllers.UserController{}, "GET:List")
 	beego.Router("/user/add", &controllers.UserController{}, "POST:Add")
 	beego.Router("/upload", &controllers.FormController{}, "POST:Upload")
+	beego.Get("/test", func(context *context.Context) {
+		context.Output.Body([]byte("heeo"))
+	})
 }
